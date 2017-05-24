68a8ca9f93de2f886ab0abad50827192b696c6cc
shiramir <shiramiremail@gmail.com>
2017-05-24 16:09:14 +0300

    Improved Tracking Algorithm
    Chose contour in correlation to the former location of the object.

--- src/gui.py
+++ src/gui.py
@@ -3,6 +3,7 @@ from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import askdirectory
 import cv2
 import os 
+import tracker
 
 BACKGROUND = '#f5f5f5'
 BUTTONS = '#ffffff'
@@ -15,39 +16,20 @@ def load_video():
     #video_fn = askopenfilename(initialdir=r'C:\Users\student\Documents\GitHub\K9andFriends\videos')
     video_fn = askopenfilename(initialdir=dir_path)
 
-def do_test():
+def start():
     """ callback function """
-    training_msg.set("")
-    res_path = test.run_testing(test_img_path.get(), output_dir.get(), float(thresh.get()))
-    """res = PIL.Image.open(res_path)
-    res = PIL.ImageTk.PhotoImage(res)
-    # image = Label(canvas, image=res, anchor=CENTER)
-    image.configure(image=res)
-    image.image = res
-    image.grid(row=2, column=2, columnspan=3, rowspan=12)
-    """
-
-def add_training_set():
-    """ callback function """
-    training_msg.set("")
-    new_training_set_path.set(askdirectory())
-    train.add_training_set(new_training_set_path.get())
-
-def set_image_path():
-    """ callback function """
-    training_msg.set("")
-    test_img_path.set(askopenfilename())
+    tracker.track(video_fn.get(), dog_min, dog_max, robot_min, robot_max)
 
 def set_output_dir():
     """ callback function """
-    training_msg.set("")
     output_dir.set(askdirectory())
 
 root = Tk()
-root.title("K9 and Robot! :)")
+root.title("K9-Robot")
 root.configure(background=BACKGROUND)
 w, h = root.winfo_screenwidth(), root.winfo_screenheight()
-root.geometry("%dx%d+0+0" % (w, h))
+#root.geometry("%dx%d+0+0" % (w, h))
+root.geometry('{}x{}'.format(80, root.winfo_screenheight()))
 
 root.focus_set() # move focus to this widget
 root.bind("<Escape>", lambda e: e.widget.quit())
@@ -64,46 +46,43 @@ robot_max = StringVar()
 robot_min = StringVar()
 
 # Header
-headline = Label(root, font="Gisha 20 bold", bg=BACKGROUND, fg=TEXT, text="K9 and Robot! :)", anchor=CENTER)
+headline = Label(root, font="Gisha 20 bold", bg=BACKGROUND, fg=TEXT, text="K9-Robot", anchor=CENTER)
 headline.grid(row=0, column=0, columnspan=7, padx=10, pady=10)
 
 # Left input and output settings
-btn1 = Button(root, font="Gisha 12", fg=BUTTONS, bg=TEXT, command=load_video, text="load video", width=12)
-btn1.grid(row=1, column=0, padx=15, pady=0)
-btn2 = Button(root, font="Gisha 12", fg=BUTTONS, bg=TEXT, command=do_test, text="play", width=12)
-btn2.grid(row=2, column=0, padx=15, pady=0)
-btn3 = Button(root, font="Gisha 12", fg=BUTTONS, bg=TEXT, command=add_training_set, text="pause", width=12)
-btn3.grid(row=3, column=0, padx=15, pady=0)
-btn4 = Checkbutton(root, font="Gisha 12", fg=TEXT, bg=BACKGROUND, variable=paths, text="show paths", width=10)
-btn4.grid(row=4, column=0, padx=15, pady=0)
-btn5 = Checkbutton(root, font="Gisha 12", fg=TEXT, bg=BACKGROUND, variable=boxes, text="show boxes", width=10)
-btn5.grid(row=5, column=0, padx=15, pady=0)
-btn6 = Checkbutton(root, font="Gisha 12", fg=TEXT, bg=BACKGROUND, variable=distance, text="show distance", width=10)
-btn6.grid(row=6, column=0, padx=15, pady=0)
-
-# Main frame for showing results
-canvas = Canvas(bg=BUTTONS, bd=5, width=0.75*w, height=0.75*h).grid(row=1, column=1, columnspan=5, rowspan=22, sticky=E)
-image = Label(canvas, image="", anchor=CENTER)
-
-# Right parameter settings
+btn1 = Button(root, font="Gisha 12", fg=BUTTONS, bg=TEXT, command=load_video, text="Load Video", width=12)
+btn1.grid(row=1, column=0, padx=15, pady=5)
+video_fn.set('../videos/ana.mp4')
+btn2 = Button(root, font="Gisha 12", fg=BUTTONS, bg=TEXT, command=set_output_dir, text="Output Path", width=12)
+btn2.grid(row=2, column=0, padx=15, pady=5)
+output_dir.set('../results')
+btn3 = Button(root, font="Gisha 12", fg=BUTTONS, bg=TEXT, command=start, text="Start", width=12)
+btn3.grid(row=3, column=0, padx=15, pady=5)
 label1 = Label(root, font="Gisha 12", fg=TEXT, bg=BACKGROUND, text="dog max size:", width=15)
-label1.grid(row=1, column=6, padx=0, pady=0)
+label1.grid(row=4, column=0, padx=0, pady=0)
 entry1 = Entry(root, font="gisha 12", fg=TEXT, bg=BUTTONS, textvariable=dog_max, width=12)
-entry1.grid(row=2, column=6, padx=15, pady=0)
-dog_max.set("9")
+entry1.grid(row=5, column=0, padx=15, pady=5)
+dog_max.set("1000000")
 label2 = Label(root, font="gisha 12", fg=TEXT, bg=BACKGROUND, text="dog min size:", width=15)
-label2.grid(row=3, column=6, padx=0, pady=0)
+label2.grid(row=6, column=0, padx=0, pady=0)
 entry2 = Entry(root, font="gisha 12", fg=TEXT, bg=BUTTONS, textvariable=dog_min, width=12)
-entry2.grid(row=4, column=6, padx=15, pady=0)
-dog_min.set("9")
+entry2.grid(row=7, column=0, padx=15, pady=5)
+dog_min.set("20000")
 label3 = Label(root, font="gisha 12", fg=TEXT, bg=BACKGROUND, text="robot max size:", width=15)
-label3.grid(row=5, column=6, padx=0, pady=0)
+label3.grid(row=8, column=0, padx=0, pady=0)
 entry3 = Entry(root, font="gisha 12", fg=TEXT, bg=BUTTONS, textvariable=robot_max, width=12)
-entry3.grid(row=6, column=6, padx=15, pady=0)
-robot_max.set("9")
+entry3.grid(row=9, column=0, padx=15, pady=5)
+robot_max.set("9000")
 label4 = Label(root, font="gisha 12", fg=TEXT, bg=BACKGROUND, text="robot min size:", width=15)
-label4.grid(row=7, column=6, padx=0, pady=0)
+label4.grid(row=10, column=0, padx=0, pady=0)
 entry4 = Entry(root, font="gisha 12", fg=TEXT, bg=BUTTONS, textvariable=robot_min, width=12)
-entry4.grid(row=8, column=6, padx=15, pady=0)
-robot_min.set("9")
+entry4.grid(row=11, column=0, padx=15, pady=5)
+robot_min.set("5000")
+btn4 = Checkbutton(root, font="Gisha 12", fg=TEXT, bg=BACKGROUND, variable=paths, text="show paths", width=10)
+btn4.grid(row=12, column=0, padx=15, pady=0)
+btn5 = Checkbutton(root, font="Gisha 12", fg=TEXT, bg=BACKGROUND, variable=boxes, text="show boxes", width=10)
+btn5.grid(row=13, column=0, padx=15, pady=0)
+btn6 = Checkbutton(root, font="Gisha 12", fg=TEXT, bg=BACKGROUND, variable=distance, text="show distance", width=10)
+btn6.grid(row=14, column=0, padx=15, pady=0)
+
 root.mainloop()
--- src/tracker.py
+++ src/tracker.py
@@ -2,13 +2,52 @@ import numpy as np
 import numpy.random as rnd
 import cv2
 import obj 
+import os
+import math
 
-VIDEO = r'../videos/ana.mp4'
+def find_best_contour(tracked_obj, contours):
 
-if __name__ == "__main__":
+    min_dist = -1 
+    best_contour = contours[0]
+    for i in range(len(contours)):
+        # Find contour's centriod
+        moment = cv2.moments(contours[i])
+        if moment['m00'] == 0:
+            moment['m00'] = 0.000001
+        cx = int(moment['m10']/moment['m00'])
+        cy = int(moment['m01']/moment['m00'])
+        dist = int(np.sqrt((tracked_obj.cx - cx)**2 + (tracked_obj.cy - cy)**2))
+        area = cv2.contourArea(contours[i])
+        if (area >= tracked_obj.min_size) and (area < tracked_obj.max_size):
+            if (dist < min_dist) or (min_dist == -1):
+                best_contour = contours[i]
+                min_dist = dist
+    return best_contour, min_dist
+
+def paint_frame(frame, dog, robot, show_trail, show_boxes, show_distance):
+
+    if show_trail == 1:
+        dog.print_trail(frame)
+        robot.print_trail(frame)
+    if show_boxes == 1:
+        dog.box(frame)
+        robot.box(frame)
+    if show_distance == 1:
+        cv2.line(frame, (dog.cx, dog.cy), (robot.cx, robot.cy), (255, 0, 0), 2)
+        dist = int(np.sqrt((dog.cx - robot.cx)**2 + (dog.cy - robot.cy)**2))
+        dist_loc = (int(np.shape(frame)[1]/10),int(np.shape(frame)[0]/10))
+        cv2.putText(frame, 'Distance:' + str(dist), dist_loc, cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+def track(fn, dog_min, dog_max, robot_min, robot_max):
 
     # Initialize parameters
-    video = cv2.VideoCapture(VIDEO)
+    video = cv2.VideoCapture(fn)
+    # Find a file name that isn't taken
+    #i = 1
+    #while os.path.isfile('%s%s%d%s' % (output_dir, 'result', i, '.mp4')):
+    #    i = i + 1
+    #fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    #new_video = cv2.VideoWriter('%s%s%d%s' % (output_dir, 'result', i, '.mp4'), fourcc, 20.0, (640,480))
     fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
     dog = obj.Obj((0, 0), 'becky', 20000, 1000000)
     robot = obj.Obj((0, 0), 'robot', 5000, 9000)
@@ -25,85 +64,83 @@ if __name__ == "__main__":
 
         # Apply Background Subtraction
         fgmask = fgbg.apply(frame)
-        fgmask = cv2.GaussianBlur(fgmask, (45, 45), 0)
+        fgmask = cv2.GaussianBlur(fgmask, (35, 35), 0)
 
         # Find object contours
         _, contours, hierarchy = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        #cv2.drawContours(frame, contours, -1, (0, 0, 255), 2)
+        cv2.drawContours(frame, contours, -1, (0, 0, 255), 2)
 
-        for i in range(len(contours)):
-            if cv2.contourArea(contours[i]) >= dog.min_size:
-                # Find contour's centriod
-                moment = cv2.moments(contours[i])
-                dog.cx = int(moment['m10']/moment['m00'])
-                dog.cy = int(moment['m01']/moment['m00'])
-                # for debug -- show centriod
-                if show_trail == 1:
-                    cv2.circle(frame, (dog.cx, dog.cy), 6, (255, 0, 0), -1)
+        if len(contours) != 0:
+            best_contour, min_dist = find_best_contour(dog, contours)
+            if min_dist != -1:
+                moment = cv2.moments(best_contour)
+                if moment['m00'] == 0:
+                    moment['m00'] = 0.000001
+                dog.cx = int(moment['m10']/math.ceil(moment['m00']))
+                dog.cy = int(moment['m01']/math.ceil(moment['m00']))
                 measure = np.array([[np.float32(dog.cx)], [np.float32(dog.cy)]])
                 # Correct Measurements
                 dog.kalman.correct(measure)
-                dog.x, dog.y, dog.w, dog.h = cv2.boundingRect(contours[i])
-                break
+                dog.x, dog.y, dog.w, dog.h = cv2.boundingRect(best_contour)
 
         # Predict and show results.
         pred = dog.kalman.predict()
         dog.cx , dog.cy = pred[0], pred[1]
         dog.update_path((dog.cx, dog.cy))
-        if show_trail == 1:
-            dog.print_trail(frame)
-        if show_boxes == 1:
-            dog.box(frame)
 
-        for i in range(len(contours)):
-            if (cv2.contourArea(contours[i]) >= robot.min_size) and (cv2.contourArea(contours[i]) < robot.max_size):
-                # Find contour's centriod
-                moment = cv2.moments(contours[i])
+        if len(contours) != 0:
+            best_contour, min_dist = find_best_contour(robot, contours)
+            if min_dist != -1:
+                moment = cv2.moments(best_contour)
+                if moment['m00'] == 0:
+                    moment['m00'] = 0.000001
                 robot.cx = int(moment['m10']/moment['m00'])
                 robot.cy = int(moment['m01']/moment['m00'])
-                # for debug -- show centriod
-                if show_trail == 1:
-                    cv2.circle(frame, (robot.cx, robot.cy), 6, (255, 0, 0), -1)
                 measure = np.array([[np.float32(robot.cx)], [np.float32(robot.cy)]])
                 # Correct Measurements
                 robot.kalman.correct(measure)
-                robot.x, robot.y, robot.w, robot.h = cv2.boundingRect(contours[i])
-                break
+                robot.x, robot.y, robot.w, robot.h = cv2.boundingRect(best_contour)
 
         # Predict and show results.
         pred = robot.kalman.predict()
         robot.cx , robot.cy = pred[0], pred[1]
         robot.update_path((robot.cx, robot.cy))
-        if show_trail == 1:
-            robot.print_trail(frame)
-        if show_boxes == 1:
-            robot.box(frame)
-        
-        if show_distance == 1:
-            cv2.line(frame, (dog.cx, dog.cy), (robot.cx, robot.cy), (255, 0, 0), 2)
-            dist = int(np.sqrt((dog.cx - robot.cx)**2 + (dog.cx - robot.cx)**2))
-            dist_loc = (int(np.shape(frame)[1]/10),int(np.shape(frame)[0]/10))
-            cv2.putText(frame, 'Distance:' + str(dist), dist_loc, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
 
         # Display the resulting frame
-        cv2.imshow('frame', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+        disp_frame = frame.copy()
+        paint_frame(disp_frame, dog, robot, show_trail, show_boxes, show_distance)
+        cv2.imshow('frame', cv2.cvtColor(disp_frame, cv2.COLOR_BGR2RGB))
         #cv2.imshow('frame', fgmask)
 
-        key = cv2.waitKey(60) & 0xFF 
-        if key == ord('q'):
-            break
-        elif key == ord('p'):
-            key = '0'
-            while key != ord('p'):
-                key = cv2.waitKey(0)
-        elif key == ord('t'):
+        key = cv2.waitKey(60) & 0xFF
+        if key == ord('t'):
             show_trail = 1 - show_trail
         elif key == ord('b'):
             show_boxes = 1 - show_boxes
         elif key == ord('d'):
             show_distance = 1 - show_distance
+        elif key == ord(' '):
+            key = '0'
+            while key != ord(' ') and key != ord('q'):
+                if key == ord('q'):
+                    break
+                elif key == ord('t'):
+                    show_trail = 1 - show_trail
+                elif key == ord('b'):
+                    show_boxes = 1 - show_boxes
+                elif key == ord('d'):
+                    show_distance = 1 - show_distance
+                disp_frame = frame.copy()
+                paint_frame(disp_frame, dog, robot, show_trail, show_boxes, show_distance)
+                cv2.imshow('frame', cv2.cvtColor(disp_frame, cv2.COLOR_BGR2RGB))
+                key = cv2.waitKey(0)
+        if key == 27: #Esc ASCII code
+            break
         key = '0'
 
     # When everything is done, release the capture
     video.release()
     cv2.destroyAllWindows()
+    
+if __name__ == "__main__":
+    track('../videos/ana.mp4', 20000, 1000000, 5000, 9000)
